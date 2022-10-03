5461e83ee071b1e3fd220b82f23e4af125813434
Jay <click4mail21@gmail.com>
2022-10-03 16:51:20 +0530

    eureka server for spring boot micro service

---
+++ src/main/java/com/api/eserver/MicroServiceEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.api.eserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroServiceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceEurekaServerApplication.class, args);
+	}
+
+}
